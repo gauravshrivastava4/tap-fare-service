fd65f69096d1633236a3b068c764dcf026f480ba
gauravshrivastava4 <52674600+gauravshrivastava4@users.noreply.github.com>
2024-09-14 18:46:11 +1000

    Project setup

---
+++ src/main/java/com/littlepay/tapfare/TapfareApplication.java
@@ -0,0 +1,13 @@
+package com.littlepay.tapfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapfareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapfareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/littlepay/tapfare/TapfareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlepay.tapfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapfareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
