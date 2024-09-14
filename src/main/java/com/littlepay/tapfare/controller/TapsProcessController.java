077e214cd87382cea186956bac3dbdb8c42c5077
gauravshrivastava4 <gauravshrivastava4@gmail.com>
2024-09-14 20:11:47 +1000

    Adds TapsProcessController to receive requests

---
+++ src/main/java/com/littlepay/tapfare/controller/TapsProcessController.java
@@ -0,0 +1,22 @@
+package com.littlepay.tapfare.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/taps")
+@RequiredArgsConstructor
+@Slf4j
+public class TapsProcessController {
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/process")
+    public String processTaps() {
+        return "OK";
+    }
+}
